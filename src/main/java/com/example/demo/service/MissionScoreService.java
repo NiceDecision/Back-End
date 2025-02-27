ed046347144a95116d77a95853216ddf0b438cc1
wcorn <dsk08208@gmail.com>
2025-02-27 15:25:51 +0900

    fix: ranking 이름 null 수정

--- src/main/java/com/example/demo/service/MissionScoreService.java
+++ src/main/java/com/example/demo/service/MissionScoreService.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import com.example.demo.domain.Mission;
 import com.example.demo.domain.Ranking;
 import com.example.demo.domain.User;
+import com.example.demo.dto.GetTop5GameScoresResponse;
 import com.example.demo.dto.MissionDto;
 import com.example.demo.repository.MissionRepository;
 import com.example.demo.repository.RankingRepository;
 import com.example.demo.repository.UserRepository;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,22 +41,23 @@ public class MissionScoreService {
         Mission mission = new Mission(userId, missionCnt, missionPnt);
         missionRepository.save(mission);
 
-        // User의 name을 가져오기
-        Optional<User> optionalUser = userRepository.findById(userId);
-        String userName = optionalUser.map(User::getName).orElse("Unknown");
-
         // totalPnt 계산 후 Ranking 테이블에 저장
         int totalPnt = missionCnt * missionPnt;
         Ranking ranking = new Ranking();
         ranking.setUserId(userId);
         ranking.setTotalPnt(totalPnt);
-        ranking.setName(userName); // User의 name 설정
 
         return rankingRepository.save(ranking);
     }
 
     // 상위 5개 랭킹 조회
-    public List<Ranking> getTop5GameScores() {
-        return rankingRepository.findTop5ByOrderByTotalPntDesc();
+    public List<GetTop5GameScoresResponse> getTop5GameScores() {
+        List<Ranking> rankings =  rankingRepository.findTop5ByOrderByTotalPntDesc();
+        List<GetTop5GameScoresResponse> responses = new ArrayList<>();
+        for(int i = 0; i < rankings.size(); i++) {
+            User user = userRepository.findById(rankings.get(i).getUserId()).get();
+            responses.add(GetTop5GameScoresResponse.of(i+1,user.getName(),rankings.get(i).getTotalPnt()));
+        }
+        return responses;
     }
 }
